af8aa0fb5e91c407cce1407eab3e4d8bd2df4f8c
Rodion <rodionchik17@gmail.com>
2025-03-09 16:53:48 +0200

    initial api-gateway

---
+++ api-gateway/src/test/java/microservice/module/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package microservice.module.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
