a96999ceebb43f22742a238b0e010023bdd28903
Rodion <rodionchik17@gmail.com>
2025-03-09 17:10:55 +0200

    created eureka-client-file-storage module

---
+++ eureka-client-file-storage/src/main/java/microservice/module/eurekaclientfilestorage/EurekaClientFileStorageApplication.java
@@ -0,0 +1,13 @@
+package microservice.module.eurekaclientfilestorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaClientFileStorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientFileStorageApplication.class, args);
+    }
+
+}
---
+++ eureka-client-file-storage/src/test/java/microservice/module/eurekaclientfilestorage/EurekaClientFileStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package microservice.module.eurekaclientfilestorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaClientFileStorageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
