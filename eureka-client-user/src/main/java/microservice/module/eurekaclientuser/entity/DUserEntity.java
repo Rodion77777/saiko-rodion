7decf158a47c829af32212fd9162231e114a04c8
Rodion <rodionchik17@gmail.com>
2025-03-12 00:48:38 +0200

    Созданы объекты для правильной настройки и работы Spring Security. Имя сущности временное, что бы отличать от уже существующей

---
+++ eureka-client-user/src/main/java/microservice/module/eurekaclientuser/entity/DUserEntity.java
@@ -0,0 +1,73 @@
+package microservice.module.eurekaclientuser.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "user_entity_details")
+public class DUserEntity implements UserDetails {
+
+    /**
+     * В ДОРАБОТКЕ
+     */
+
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @SequenceGenerator(name = "user_id_seq", sequenceName = "user_id_seq", allocationSize = 1)
+    private Long id;
+
+    @Column(name = "username", unique = true, nullable = false)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+
+    @ElementCollection
+    private List<Long> todoId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+}
---
+++ eureka-client-user/src/main/java/microservice/module/eurekaclientuser/entity/Role.java
@@ -0,0 +1,6 @@
+package microservice.module.eurekaclientuser.entity;
+
+public enum Role {
+    ROLE_USER,
+    ROLE_ADMIN
+}
